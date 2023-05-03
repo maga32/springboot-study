22a53ed06057963e5bece25372f4d79f591b3bc0
seunghwa <maga32@naver.com>
2023-05-03 11:00:39 +0900

    frontController -> DispatcherServlet 대체 / 어노테이션 적용으로 컨트롤러 동작

--- src/main/java/springboot/helloboot/HellobootApplication.java
+++ src/main/java/springboot/helloboot/HellobootApplication.java
@@ -3,54 +3,30 @@ package springboot.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		/*--------- 스프링 컨테이너 작성 및 빈 초기화 시작 ---------*/
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		// application Context에 bean 등록 및 의존관계등록(알아서해줌)
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		// 실제 생성
 		applicationContext.refresh();
+		/*--------- 스프링 컨테이너 작성 및 빈 초기화  끝 ---------*/
 
+		/*--------- 서블릿 컨테이너 실행 및 서블릿 등록 시작 ---------*/
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontController", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-					/* 인증, 보안, 다국어, 공통기능 작성 */
-
-					// 컨트롤러 분배기능
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-
-						String ret = helloController.hello(name);
-
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*");
+			servletContext.addServlet("dispatcherServlet",
+				new DispatcherServlet(applicationContext)
+			).addMapping("/*");
 		});
 		webServer.start();
+		/*--------- 서블릿 컨테이너 실행 및 서블릿 등록 끝 ---------*/
 	}
 
 }
