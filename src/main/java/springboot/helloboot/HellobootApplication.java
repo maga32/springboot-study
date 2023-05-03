081c8eee4f7fc28e545fe1c3b4d27831fc0e265f
seunghwa <maga32@naver.com>
2023-05-03 11:15:38 +0900

    @Configuration @Bean 어노테이션 등록으로 빈등록(applicationContext -> AnnotationConfigWebApplicationContext 변경)

--- src/main/java/springboot/helloboot/HellobootApplication.java
+++ src/main/java/springboot/helloboot/HellobootApplication.java
@@ -3,14 +3,26 @@ package springboot.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		/*--------- 스프링 컨테이너 작성 및 빈 초기화 시작 ---------*/
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -26,9 +38,8 @@ public class HellobootApplication {
 				/*--------- 서블릿 컨테이너 실행 및 서블릿 등록 끝 ---------*/
 			}
 		};
-		// application Context에 bean 등록 및 의존관계등록(알아서해줌)
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		// applicationContext 내부의 bean 등록 및 의존관계등록(알아서해줌)
+		applicationContext.register(HellobootApplication.class);
 		// 실제 생성
 		applicationContext.refresh();
 		/*--------- 스프링 컨테이너 작성 및 빈 초기화  끝 ---------*/
