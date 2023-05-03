f3e466869eece7ae752f680a7d5d3067df7911fd
seunghwa <maga32@naver.com>
2023-05-03 15:50:45 +0900

    @Configuration, @ComponentScan, @Component 로 빈등록하기

--- src/main/java/springboot/helloboot/HellobootApplication.java
+++ src/main/java/springboot/helloboot/HellobootApplication.java
@@ -3,22 +3,14 @@ package springboot.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class HellobootApplication {
-	@Bean
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
-	}
-
-	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
-	}
 
 	public static void main(String[] args) {
 		/*--------- 스프링 컨테이너 작성 및 빈 초기화 시작 ---------*/
@@ -31,7 +23,7 @@ public class HellobootApplication {
 				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
 					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
+						new DispatcherServlet(this)
 					).addMapping("/*");
 				});
 				webServer.start();
