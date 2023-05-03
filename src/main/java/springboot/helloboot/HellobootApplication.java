7792f1e6ed4399896b79c6eab973337d06108d6d
seunghwa <maga32@naver.com>
2023-05-03 16:12:54 +0900

    작동함수를 스프링처럼 아예 다른 Class로 보냄

--- src/main/java/springboot/helloboot/HellobootApplication.java
+++ src/main/java/springboot/helloboot/HellobootApplication.java
@@ -23,34 +23,7 @@ public class HellobootApplication {
 	}
 
 	public static void main(String[] args) {
-		run(HellobootApplication.class, args);
-	}
-
-	private static void run(Class<?> applicationClass, String... args) {
-		/*--------- 스프링 컨테이너 작성 및 빈 초기화 시작 ---------*/
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				/*--------- 서블릿 컨테이너 실행 및 서블릿 등록 시작 ---------*/
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-				// 스프링 컨테이너 주입
-				dispatcherServlet.setApplicationContext(this);
-
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
-				});
-				webServer.start();
-				/*--------- 서블릿 컨테이너 실행 및 서블릿 등록 끝 ---------*/
-			}
-		};
-		// applicationContext 내부의 bean 등록 및 의존관계등록(알아서해줌)
-		applicationContext.register(applicationClass);
-		// 실제 생성
-		applicationContext.refresh();
-		/*--------- 스프링 컨테이너 작성 및 빈 초기화  끝 ---------*/
+		MySpringApplication.run(HellobootApplication.class, args);
 	}
 
 }
