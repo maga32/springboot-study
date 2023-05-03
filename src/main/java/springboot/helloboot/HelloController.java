f36edc9f2fa3bc184284432ed73a47a1735a1938
seunghwa <maga32@naver.com>
2023-05-03 16:12:19 +0900

    main 밖으로 작동함수 빼냄

--- src/main/java/springboot/helloboot/HelloController.java
+++ src/main/java/springboot/helloboot/HelloController.java
@@ -1,24 +1,29 @@
 package springboot.helloboot;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping("/hello")
-@Component
+@RequestMapping
+@RestController
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+        System.out.println(applicationContext);
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
 }
--- src/main/java/springboot/helloboot/HellobootApplication.java
+++ src/main/java/springboot/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package springboot.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -11,8 +12,21 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 
 	public static void main(String[] args) {
+		run(HellobootApplication.class, args);
+	}
+
+	private static void run(Class<?> applicationClass, String... args) {
 		/*--------- 스프링 컨테이너 작성 및 빈 초기화 시작 ---------*/
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
@@ -20,23 +34,23 @@ public class HellobootApplication {
 				super.onRefresh();
 
 				/*--------- 서블릿 컨테이너 실행 및 서블릿 등록 시작 ---------*/
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				// 스프링 컨테이너 주입
+				dispatcherServlet.setApplicationContext(this);
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-						new DispatcherServlet(this)
-					).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
 				});
 				webServer.start();
 				/*--------- 서블릿 컨테이너 실행 및 서블릿 등록 끝 ---------*/
 			}
 		};
 		// applicationContext 내부의 bean 등록 및 의존관계등록(알아서해줌)
-		applicationContext.register(HellobootApplication.class);
+		applicationContext.register(applicationClass);
 		// 실제 생성
 		applicationContext.refresh();
 		/*--------- 스프링 컨테이너 작성 및 빈 초기화  끝 ---------*/
-
-
 	}
 
 }
